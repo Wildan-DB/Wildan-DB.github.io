2de6fd0767b7d24d5c3cd40d4e35b89bd4c66717
Wildan-DB <109508018+Wildan-DB@users.noreply.github.com>
2022-07-18 15:10:25 +0700

    Delete one.js

--- one.js
+++
@@ -1,8 +0,0 @@
-function init() {
-  Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1j-Ymd_GCQNjRKsPjs7FRCxkHcFj30YUwL5ko1tdsQl0/pubhtml',
-                    callback: function(data, tabletop) { 
-                      console.log(data)
-                    },
-                    simpleSheet: true } )
-}
-window.addEventListener('DOMContentLoaded', init)
